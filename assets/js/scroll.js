10b2170e27c7ec1d36c6879cb970d1ca741978a4
Matheus Baum <76802796+matheusbaum@users.noreply.github.com>
2023-01-16 14:04:44 -0300

    new commit

---
+++ assets/js/scroll.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '15px' });
+sr.reveal('.imagem', {duration : 2000, origin: 'right'})
+sr.reveal('.descricao', {duration : 2000, origin: 'left'})
+sr.reveal('.linkedin', {duration : 2000,  origin: 'left'})
+sr.reveal('.minha-foto', {duration : 2000,  origin: 'left'})
+sr.reveal('.sobre-mim', {duration : 2000, origin:'right'})
+sr.reveal('.projetos-itens', {duration : 2000, origin:'bottom'})
+sr.reveal('.project', {duration : 2000, origin:'bottom'})
+sr.reveal('.github-link', {duration : 2000, origin:'bottom'})
+sr.reveal('.conteiner-skills', {duration : 2000, origin:'left'})
+sr.reveal('.conteiner-descricao', {duration : 2000, origin:'right'})
+sr.reveal('h3', {duration : 2000, origin:'right'})
+sr.reveal('h2', {duration : 2000, origin:'right'})
+sr.reveal('.contato', {duration : 2000, origin:'right'})
+sr.reveal('.form', {duration : 2000, origin:'right'})
