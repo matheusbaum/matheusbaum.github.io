c124c4afa4814982500a93251a0359907b6aa1e8
Matheus Baum <76802796+matheusbaum@users.noreply.github.com>
2024-10-03 13:13:02 -0300

    Update acao.js

--- assets/js/acao.js
+++ assets/js/acao.js
@@ -1,22 +1,22 @@
-const botaoVerMais = document.querySelector("button");
-const habilidades = document.querySelectorAll(".escondido");
+const botaoVerMais = document.querySelector('button');
+const habilidades = document.querySelectorAll('.escondido');
 
-function cliqueVer(e) {
-  habilidades.forEach((elemento) => {
-    if (elemento.classList.contains("escondido")) {
-      elemento.classList.remove("escondido");
-      elemento.classList.add("visivel");
-      botaoVerMais.innerHTML = "Ver menos";
-    } else {
-      elemento.classList.add("escondido");
-      elemento.classList.remove("visivel");
-      botaoVerMais.innerHTML = "Ver mais";
-    }
-  });
+function cliqueVer (e) {
+    habilidades.forEach( (elemento) => {
+        if (elemento.classList.contains('escondido')) {
+            elemento.classList.remove('escondido');
+            elemento.classList.add('visivel');
+            botaoVerMais.innerHTML = 'Ver menos';
+        } else {
+            elemento.classList.add('escondido');
+            elemento.classList.remove('visivel');
+            botaoVerMais.innerHTML = 'Ver mais';
+        }
+    } )
 }
 
-function maisHabilidades(elemento) {
-  elemento.addEventListener("click", cliqueVer);
+function maisHabilidades (elemento) {
+    elemento.addEventListener('click', cliqueVer)
 }
 
 maisHabilidades(botaoVerMais);
